a0f19c88dcfaea7da296bc502787345fd0cb7c1f
wc_earlymor <1162907534@qq.com>
2023-04-22 21:02:27 +0800

    reference

--- reference.cpp
+++ reference.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 void swap(int &a, int &b) {
-  int t=a;
-  a=b;
-  b=t;
+  int t = a;
+  a = b;
+  b = t;
   cout << "a=" << a << endl;
   cout << "b=" << b << endl;
 }
@@ -11,12 +11,12 @@ int main() {
   int a = 10;
   int c = 30;
   int &b = a; // b是对a的别名，本质是指针常量
-  // 即int &a=b; =>const int * a = &b;
+  // 即int &a=b; => int *const a = &b;
   b = 20;
   // 即a=10;=>*a=10;
   cout << "a=" << a << endl;
   cout << "b=" << b << endl;
-  swap(a,c);
+  swap(a, c);
   cout << "a=" << a << endl;
   cout << "b=" << b << endl;
   return 0;
