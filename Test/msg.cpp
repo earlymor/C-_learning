ec4d618aa50ced024b5a8d0fbd046734c5131268
wc_earlymor <1162907534@qq.com>
2023-08-18 00:07:35 +0800

    input

--- Test/msg.cpp
+++ Test/msg.cpp
@@ -1,21 +1,51 @@
-#include <iostream>
+#include <unistd.h>
 #include <chrono>
+#include <iostream>
+#include <string>
 #include <thread>
 
-int main() {
-    std::string messages[] = {
-        "Hello, how are you?",
-        "I'm good, thank you!",
-        "What are you up to?"
-    };
+using namespace std;
+
+bool dataInput(int& data) {
+    if (!(cin >> data)) {
+        // 清除错误状态和缓冲区
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "输入无效。" << std::endl;
+        sleep(1);
+        cout << "\033[A"
+             << "\33[2K\r";
+        return false;
+    }
+    cin.get();
+    return true;
+}
+void test1() {
+    std::string messages[] = {"Hello, how are you?", "I'm good, thank you!",
+                              "What are you up to?"};
 
     for (const std::string& message : messages) {
-        std::cout << "\33[2K\r";
-        std::cout << "\033[K" << message << std::flush;
+        std::cout << message << std::flush;
         std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "\33[2K\r"
+                  << "\033[K";
     }
 
     std::cout << std::endl;
+}
+void test2() {
+    int number;
+    bool legal = false;
+    while (!legal) {
+        system("clear");
+        cout << "请输入好友账号："<<std::flush;
+        legal = dataInput(number);
+        cout << "\033[A"
+             << "\33[2K\r" ;
+    }
+}
 
-    return 0;
+int main() {
+    test2();
+        return 0;
 }
